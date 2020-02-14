e6b1c1892900dc1dcf145323edb171572f391aa3
morley gwapo <moravel752@gmail.com>
2020-02-14 08:30:53 +0800

    setup dashboard

---
+++ resources/views/user/side.blade.php
@@ -0,0 +1,31 @@
+<aside>
+      <div id="sidebar" class="nav-collapse ">
+      
+        <ul class="sidebar-menu">
+          <li class="active">
+            <a class="" href="#">
+                  <i class="icon_house_alt"></i>
+                  <span>Home</span>
+            </a>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;" class="">
+                          <i class="icon_document_alt"></i>
+                          <span>Users</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+            <ul class="sub">
+              <li><a class="" href="#">Staff</a></li>
+             
+            </ul>
+          </li>
+          
+         
+
+         
+        
+          
+        </ul>
+        
+      </div>
+    </aside>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,52 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
 
-        <title>Laravel</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Creative - Bootstrap 3 Responsive Admin Template">
+  <meta name="author" content="GeeksLabs">
+  <meta name="keyword" content="Creative, Dashboard, Admin, Template, Theme, Bootstrap, Responsive, Retina, Minimal">
+  <link rel="shortcut icon" href="img/favicon.png">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+  <title>Water Billing System</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
+  <link href="{{URL::to('dashboard/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{URL::to('dashboard/css/bootstrap-theme.css')}}" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="{{URL::to('dashboard/css/elegant-icons-style.css')}}">
+  
+  <link href="{{URL::to('dashboard/css/widgets.css')}}" rel="stylesheet">
+  <link href="{{URL::to('dashboard/css/style.css')}}" rel="stylesheet">
+ 
+  {{-- <script src="{{URL::to('tables/jquery-1.12.3.js')}}"></script>
+  <script src="{{URL::to('dashboard/js/bootstrap.min.js')}}"></script> --}}
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  @yield('styles')
+</head>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+<body>
+ 
+  <section id="container" class="">
+  
+    @include('user.nav')
+    @include('user.side')
+   
+    <section id="main-content">
+      <section class="wrapper">
+       @yield('contents')
+      </section>
+    </section>
+  
 
-            .position-ref {
-                position: relative;
-            }
+ 
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+</body>
 
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+  
+  <script src="{{URL::to('dashboard/js/jquery.scrollTo.min.js')}}"></script>
+  <script src="{{URL::to('dashboard/js/jquery.nicescroll.js')}}"></script>
+  <script src="{{URL::to('dashboard/js/scripts.js')}}"></script>
+  @yield('scripts');
 </html>
